80c87673c7e6e49b9e96c73e8115989536bd2325
mookum <mookum@codejunkie.be>
2021-09-28 23:05:25 +0200

    Revert ingress-srv.yaml Update Index.ts Rewrite signout.ts

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,18 +2,19 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from "cookie-session"
+import cookieSession from 'cookie-session'
 
-import { currentUserRouter} from "./routes/current-user";
-import { signinRouter} from "./routes/signin";
-import { signoutRouter} from "./routes/signout";
-import { signupRouter} from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { currentUserRouter} from "./routes/current-user"
+import { signinRouter} from "./routes/signin"
+import { signoutRouter} from "./routes/signout"
+import { signupRouter} from "./routes/signup"
+import { errorHandler } from "./middlewares/error-handler"
+import { NotFoundError } from "./errors/not-found-error"
 
 const app = express()
 app.set('trust proxy', true)
 app.use(json())
+
 app.use(
   cookieSession({
     signed: false,
@@ -26,7 +27,7 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', async (req, res, next) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError()
 })
 
