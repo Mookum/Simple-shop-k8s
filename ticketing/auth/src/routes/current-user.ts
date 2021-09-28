27293bc23b47791ed932ccabeaf7f1f50c3b0a45
mookum <mookum@codejunkie.be>
2021-09-28 21:01:54 +0200

    initial commit

---
+++ ticketing/auth/src/routes/current-user.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const router = express.Router()
+
+router.get('/api/users/currentuser', (req, res) => {
+
+  // !req.session || !req.session?.jwt
+  // Ts shorthand !req.session?.jwt
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    )
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
+})
+
+export { router as currentUserRouter }
