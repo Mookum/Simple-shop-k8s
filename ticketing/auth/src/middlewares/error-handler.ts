1ca09029d8e9a57d2b8f05a577f2baf8a8d882e9
mookum <mookum@codejunkie.be>
2021-09-30 21:03:45 +0200

    Add NextFunction to avoid issues with post Error: Invalid request parameters

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from "express"
 import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   if(err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
