811c5e41844b20118bc1ec7a65f96d1f2eeb2f73
mookum <mookum@codejunkie.be>
2021-10-04 20:06:52 +0200

    TDD routes for ticketing

---
+++ ticketing/tickets/src/routest/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest"
+import { app } from '../../app'
+
+it('should has a route handler listening to /api/tickets for post request', function () {
+
+});
+
+it('should only be accessible if the user is logged in', function () {
+
+});
+
+it('should return an error if an invalid title is provided', function () {
+
+});
+
+it('should return nan error if an invalid price is provided', function () {
+
+});
+
+it('should create a ticket with valid inputs', function () {
+
+});
